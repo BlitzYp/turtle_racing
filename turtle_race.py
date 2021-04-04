d41e7be71643adfedd9d68e430bb391b5603b95f
BlitzYp <63169496+BlitzYp@users.noreply.github.com>
2021-04-04 22:53:52 +0300

    Add files via upload

---
+++ turtle_race.py
@@ -0,0 +1,50 @@
+import turtle
+from typing import List
+from random import choice, randint
+from time import sleep
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+COLORS = ["red", "green", "yellow", "blue", "orange"]
+
+def arrange(competitors: List[turtle.Turtle]) -> None:
+    prev_y = (-SCREEN_HEIGHT / 2) + 30
+    chosen = []
+    for i in competitors:
+        res = choice(list(filter(lambda x: not x in chosen, COLORS)))
+        chosen.append(res)
+        i.color(res)
+        i.speed("normal")
+        i.penup()
+        i.setpos((-SCREEN_WIDTH / 2) + 30, prev_y)
+        prev_y += 130
+
+def race(competitors: List[turtle.Turtle]) -> str:
+    current_max = competitors[0].xcor()
+    winner = None
+    while current_max < (SCREEN_WIDTH / 2) - 30:
+        for t in competitors:
+            t.fd(randint(5, 20))
+            if t.xcor() > current_max:
+                current_max = t.xcor()
+                winner = t.color()[0]
+    return winner
+
+if __name__ == "__main__":
+    screen = turtle.Screen()
+    while True:
+        competitors = [turtle.Turtle("turtle") for _ in range(5)]
+        screen.setup(SCREEN_WIDTH, SCREEN_HEIGHT)
+        screen.title("Turtle racing")
+        arrange(competitors)
+        data = turtle.textinput(title="Bet", prompt="Who will win: ")
+        if not len(data) or not data in COLORS: 
+            screen.clearscreen()
+            continue
+        winner = race(competitors)
+        if winner.lower() == data.lower(): print(f"Your turtle won! The winner turtle was {winner}")
+        else: print(f"Your turtle lost! The winner turtle was {winner}")
+        sleep(2)  
+        screen.clearscreen() 
+    turtle.mainloop()
